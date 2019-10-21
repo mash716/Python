606c9aab12a41eea8075581b7ccfc4d4cc1f673c
mash716 <50168435+mash716@users.noreply.github.com>
2019-10-22 07:35:29 +0900

    inter

---
+++ integer/integer0001.py
@@ -0,0 +1,6 @@
+#四則演算（加算・減算・乗算・除算）は下記の通りです。
+test_integer = 100
+print(test_integer + 10)
+print(test_integer - 10)
+print(test_integer * 10)
+print(test_integer / 10)
---
+++ integer/integer0002.py
@@ -0,0 +1,3 @@
+#数値(整数)への変換はintを使用します。
+test_str = '100'
+print(int(test_str) + 100)
