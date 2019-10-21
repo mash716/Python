c86daae4a76fe34860415d94ab65cde67d5b80c4
mash716 <50168435+mash716@users.noreply.github.com>
2019-10-22 07:45:50 +0900

    int 4

---
+++ integer/integer0004.py
@@ -0,0 +1,10 @@
+#複素数を表すcomplexは、数値へjやJを付与することで虚数になります。
+#次の例は実数が100、虚数が5の複素数です。
+#※  虚数：2乗したときに0未満になるもの(iともいわれる)
+#※1 i(imaginaryの略で実際に存在しない架空のもの)
+
+test_complex = 100 + 5j
+
+print(test_complex)
+print(test_complex.real)
+print(test_complex.imag)
