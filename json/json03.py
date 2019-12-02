d716435c7b80ddac59ededa5c6737a007cde4242
mash716 <50168435+mash716@users.noreply.github.com>
2019-12-02 22:07:24 +0900

    python666

--- json/json03.py
+++ json/json03.py
@@ -1,24 +1,31 @@
 import json
 import subprocess
 
-f = open("json01.json", 'r')
+def jsondata():
+    f = open("json01.json", 'r')
 
-#jsonの読み込み
-json_data = json.load(f)
-#確認
-#jsonファイルから固定値を取得
-A = json_data["yamada"]["aaa"]
-B = json_data["yamada"]["bbb"]
-C = json_data["yamada"]["ccc"]
-D = json_data["yamada"]["ddd"]
+    #jsonの読み込み
+    json_data = json.load(f)
+    #確認
+    #jsonファイルから固定値を取得
+    A = json_data["yamada"]["aaa"]
+    B = json_data["yamada"]["bbb"]
+    C = json_data["yamada"]["ccc"]
+    D = json_data["yamada"]["ddd"]
 
-print(A)
-print(B)
-print(C)
-print(D)
+    print(A)
+    print(B)
+    print(C)
+    print(D)
 
-print(A+B+C+D)
+    print(A+B+C+D)
 
-#実行コマンドを記入
-cmd = "aaaa.py" + " -h"
-subprocess.run(cmd,shell =True)
+    #実行コマンドを記入
+    cmd = "aaaa.py" + " -h"
+    subprocess.run(cmd,shell =True)
+
+def main():
+    jsondata()
+
+if __name__ == "__main__":
+    main()
